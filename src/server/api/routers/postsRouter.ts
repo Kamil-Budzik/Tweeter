326faff54202663967f3111f7caf807b1cb89024
DonquixoteSabo <mixboksu@gmail.com>
2023-04-04 09:47:13 +0200

    [BE] Connect post data to the user, Update Post model

--- src/server/api/routers/postsRouter.ts
+++ src/server/api/routers/postsRouter.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { clerkClient, type User } from "@clerk/nextjs/api";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -8,6 +17,19 @@ export const postsRouter = createTRPCRouter({
       take: 100,
       orderBy: [{ createdAt: "desc" }],
     });
-    return posts;
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log(users);
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
 });
