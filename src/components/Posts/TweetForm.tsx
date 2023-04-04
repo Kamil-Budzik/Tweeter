e3f0d9ff97ddffc9dbc7b89515dcf8819c381b0a
DonquixoteSabo <mixboksu@gmail.com>
2023-04-04 17:48:08 +0200

    [FE] Invalidate posts and handle mutation loading

--- src/components/Posts/TweetForm.tsx
+++ src/components/Posts/TweetForm.tsx
@@ -1,5 +1,7 @@
 import { useUser } from "@clerk/nextjs";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import { api } from "~/utils/api";
+import { LoadingSpinner } from "~/components/ui/Loading";
 
 type Inputs = {
   post: string;
@@ -7,18 +9,27 @@ type Inputs = {
 
 const TweetForm = () => {
   const { user } = useUser();
+  const ctx = api.useContext();
   const {
     register,
     handleSubmit,
     formState: { isValid },
+    reset,
   } = useForm<Inputs>();
+  const { mutate, isLoading } = api.posts.addItem.useMutation({
+    onSuccess: () => {
+      void ctx.posts.getAll.invalidate();
+      reset();
+    },
+  });
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    if (!user) return;
+    mutate({ text: data.post, authorId: user.id });
   };
 
   return (
     <form
-      onSubmit={() => handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSubmit)}
       className="mb-6 w-full max-w-3xl rounded-2xl bg-white px-4 py-2"
     >
       <div className="flex flex-col">
@@ -32,14 +43,20 @@ const TweetForm = () => {
           <img
             className="mr-2 h-16 w-16 rounded-2xl object-cover"
             src={user?.profileImageUrl}
-            alt="Your's profile image"
+            alt="Your profile image"
           />
-          <textarea
-            className="flex-grow resize-none pb-2 placeholder:font-semibold placeholder:text-[#BDBDBD]"
-            id="post"
-            placeholder="What's happening?"
-            {...register("post", { required: true })}
-          />
+          {isLoading ? (
+            <div className="flex w-full items-center justify-center">
+              <LoadingSpinner />
+            </div>
+          ) : (
+            <textarea
+              className="flex-grow resize-none pb-2 placeholder:font-semibold placeholder:text-[#BDBDBD]"
+              id="post"
+              placeholder="What's happening?"
+              {...register("post", { required: true })}
+            />
+          )}
         </div>
       </div>
 
