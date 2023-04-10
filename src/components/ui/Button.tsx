0f931cff566ab579f64fd11af55b5d5ca5b43548
DonquixoteSabo <mixboksu@gmail.com>
2023-04-10 12:03:41 +0200

    Code clean-up

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,15 @@
+import { type PropsWithChildren } from "react";
+
 interface Props {
   isDisabled?: boolean;
-  children: any;
   type?: "button" | "submit" | "reset";
 }
 
-export const Button = ({ children, isDisabled, type }: Props) => (
+export const Button = ({
+  children,
+  isDisabled,
+  type,
+}: PropsWithChildren<Props>) => (
   <button
     type={type ?? "button"}
     className="mt-6 flex items-center justify-center rounded bg-blue-500 px-6 py-2 font-medium text-white transition hover:bg-blue-600 disabled:bg-blue-300"
--- src/server/api/routers/postsRouter.ts
+++ src/server/api/routers/postsRouter.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { clerkClient, type User } from "@clerk/nextjs/api";
-import { TRPCError } from "@trpc/server";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -40,46 +39,5 @@ export const postsRouter = createTRPCRouter({
           content: input.text,
         },
       });
-    }),
-  getByUsername: publicProcedure
-    .input(z.object({ username: z.string() }))
-    .query(async ({ ctx, input }) => {
-      // const users = (
-      //   await clerkClient.users.getUserList({
-      //     limit: 100,
-      //   })
-      // ).map(filterUserForClient);
-      //
-      // console.log(users);
-      //
-      // const user = users.find((user) => user.username === input.username);
-      //
-      // if (!user)
-      //   throw new TRPCError({
-      //     code: "NOT_FOUND",
-      //     message: "Couldn't find user",
-      //   });
-
-      console.log("USERNAME", input.username);
-
-      const [user] = await clerkClient.users.getUserList({
-        username: [input.username],
-      });
-
-      if (!user) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "User not found!",
-        });
-      }
-
-      const posts = await ctx.prisma.post.findMany({
-        where: {
-          authorId: user.id,
-        },
-        take: 100,
-        orderBy: [{ createdAt: "desc" }],
-      });
-      return posts;
     }),
 });
