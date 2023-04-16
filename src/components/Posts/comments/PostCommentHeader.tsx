09f7603eb466cabda683e02f8501715b3876a852
DonquixoteSabo <mixboksu@gmail.com>
2023-04-16 14:55:23 +0200

    [FE] Export comment header to new component

---
+++ src/components/Posts/comments/PostCommentHeader.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import dayjs from "dayjs";
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
+
+interface Props {
+  postId: number;
+  username: string;
+  authorId: string;
+  commentId: number;
+  createdAt: Date;
+}
+const PostCommentHeader = ({
+  postId,
+  username,
+  authorId,
+  commentId,
+  createdAt,
+}: Props) => {
+  const { user } = useUser();
+  const ctx = api.useContext();
+  const { mutate: deleteComment } = api.comments.deleteComment.useMutation({
+    onSuccess: () => {
+      void ctx.comments.getByPostId.invalidate({ postId });
+    },
+  });
+
+  return (
+    <div className="flex items-center text-[#BDBDBD]">
+      <Link href={`/profile/${username}`}>
+        <span className="mr-2 font-semibold text-black hover:underline">
+          {username}
+        </span>
+      </Link>
+      {dayjs(createdAt).format("DD MMMM HH:mm")}
+      {user?.id === authorId && (
+        <BsFillTrash3Fill
+          onClick={() => deleteComment({ id: commentId })}
+          className="ml-2 cursor-pointer text-right text-black transition duration-500 hover:text-red-500"
+        />
+      )}
+    </div>
+  );
+};
+
+export default PostCommentHeader;
--- src/components/Posts/comments/PostCommentsList.tsx
+++ src/components/Posts/comments/PostCommentsList.tsx
@@ -1,22 +1,11 @@
-import { api } from "~/utils/api";
 import { LoadingSpinner } from "~/components/ui/Loading";
-import dayjs from "dayjs";
-import Link from "next/link";
 import ProfileImage from "~/components/ui/ProfileImage";
-
-import { BsFillTrash3Fill } from "react-icons/bs";
-import { useUser } from "@clerk/nextjs";
 import PostCommentsLikes from "~/components/Posts/comments/PostCommentsLikes";
+import PostCommentHeader from "~/components/Posts/comments/PostCommentHeader";
+import { api } from "~/utils/api";
 
 const PostCommentsList = ({ postId }: { postId: number }) => {
   const { data, isLoading } = api.comments.getByPostId.useQuery({ postId });
-  const { user } = useUser();
-  const ctx = api.useContext();
-  const { mutate: deleteComment } = api.comments.deleteComment.useMutation({
-    onSuccess: () => {
-      void ctx.comments.getByPostId.invalidate({ postId });
-    },
-  });
 
   if (!data) return <div />;
   if (isLoading) return <LoadingSpinner />;
@@ -35,20 +24,13 @@ const PostCommentsList = ({ postId }: { postId: number }) => {
               imgUrl={author?.profileImageUrl ?? ""}
             />
             <div className="break-all rounded bg-[#FAFAFA] p-2 pb-4 text-[#4F4F4F] shadow">
-              <div className="flex items-center text-[#BDBDBD]">
-                <Link href={`/profile/${author!.username}`}>
-                  <span className="mr-2 font-semibold text-black hover:underline">
-                    {author?.username}
-                  </span>
-                </Link>
-                {dayjs(comment.createdAt).format("DD MMMM HH:mm")}
-                {user?.id === author?.id && (
-                  <BsFillTrash3Fill
-                    onClick={() => deleteComment({ id: comment.id })}
-                    className="ml-2 cursor-pointer text-right text-black transition duration-500 hover:text-red-500"
-                  />
-                )}
-              </div>
+              <PostCommentHeader
+                postId={postId}
+                username={author?.username ?? ""}
+                authorId={author?.id ?? ""}
+                createdAt={comment.createdAt}
+                commentId={comment.id}
+              />
               {comment.content}
             </div>
             <PostCommentsLikes
