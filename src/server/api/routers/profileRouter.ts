d456df128fe242195492cd860724eef62a2208ed
DonquixoteSabo <mixboksu@gmail.com>
2023-05-14 17:54:49 +0200

    [BE] Add endpoint for getting follow details

--- src/server/api/routers/profileRouter.ts
+++ src/server/api/routers/profileRouter.ts
@@ -2,6 +2,16 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { INCLUDE_IN_POST } from "~/server/api/routers/postsRouter";
 import getUserByUsername from "~/server/helpers/getUserByUsername";
+import { clerkClient } from "@clerk/nextjs/api";
+import { filterUserForClientWithDetails } from "~/server/helpers/clientFilters";
+
+const ZOD_FOLLOW = z.array(
+  z.object({
+    id: z.number(),
+    userId: z.string(),
+    followedUserId: z.string(),
+  })
+);
 
 export const profileRouter = createTRPCRouter({
   // Will be used on Profile Page for following, followers, BIO
@@ -50,6 +60,27 @@ export const profileRouter = createTRPCRouter({
         },
       });
       return !!follow;
+    }),
+  getFollowData: publicProcedure
+    .input(
+      z.object({
+        followers: ZOD_FOLLOW,
+        follows: ZOD_FOLLOW,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const followersIds = input.followers.map((follower) => follower.userId);
+      const followsIds = input.follows.map((follow) => follow.followedUserId);
+      const users = (
+        await clerkClient.users.getUserList({
+          limit: 100,
+        })
+      ).map(filterUserForClientWithDetails);
+
+      return {
+        followedBy: users.filter((user) => followersIds.includes(user.id)),
+        follows: users.filter((user) => followsIds.includes(user.id)),
+      };
     }),
   toggleFollow: publicProcedure
     .input(
